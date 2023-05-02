14f15b7570215cec9670da26c49ea356a8553f10
Vini <viniciusps.1190@gmail.com>
2023-05-02 15:47:09 -0300

    Aprendendo o gitignore

---
+++ jogo2dgenerico/estudos/poo/src/Heranca.java
@@ -0,0 +1,8 @@
+public class Heranca extends Object // Object é a classe de pai de todas as classes em java, e todas a estendem,
+                                    // Apenas não é explícito, como fizemos agora. //Toda classe é passível de ter subclasses
+
+    // Logo, todas as classes, por natureza, estendem a classe object, que é a classe pai de todas as classes.
+
+{
+
+}
